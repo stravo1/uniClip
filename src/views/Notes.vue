9fcd141ef139f5ad2a83a898147cfbf80ddd7071
stravoone@msi <stravoone@gmail.com>
2021-06-01 19:31:34 +0530

    started with notes

--- src/views/Notes.vue
+++ src/views/Notes.vue
@@ -1,10 +1,5 @@
 <template>
-  <div class="notes-wrapper">
-    <p class="title is-1">sorry</p>
-    <img src="../assets/6.svg">
-    <p class="subtitle">'Notes' is under construction, will be available soon...</p>
-    <button class="button is-warning" @click="$router.replace({name:'myDevice'})">Return home</button>
-  </div>
+  
 </template>
 
 <script>
